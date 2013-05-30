61e39d737bc3fe9e9b7f4b770dfef04606eb376c
Abhishek Nanda <abnanda@microsoft.com>
2013-05-29 17:04:03 -0700

    Made changes as per code review feedback
    #1907

--- src/Microsoft.AspNet.SignalR.Client/Transports/ServerSentEventsTransport.cs
+++ src/Microsoft.AspNet.SignalR.Client/Transports/ServerSentEventsTransport.cs
@@ -215,11 +215,7 @@ namespace Microsoft.AspNet.SignalR.Client.Transports
                 {
                     callbackInvoker.Invoke((cb, token) =>
                     {
-#if NET35
-                        cb(new OperationCanceledException(Resources.Error_ConnectionCancelled));
-#else
                         cb(new OperationCanceledException(Resources.Error_ConnectionCancelled, token));
-#endif
                     }, errorCallback, disconnectToken);
                 }
             }, _request);
